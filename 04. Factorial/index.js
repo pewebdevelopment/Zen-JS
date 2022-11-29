3741acaf5e0e3501284ebf7a7f6033b151abfda3
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-29 19:01:41 +0530

    formatted JS Code

--- 04. Factorial/index.js
+++ 04. Factorial/index.js
@@ -1,15 +1,24 @@
 const factorialPara = document.getElementById("factorial");
+const input = document.getElementById("inputbox");
 
 function findfactorial(n) {
   let factorial = 1;
 
   for (i = 1; i <= n; i++) {
-    factorial *= factorial;
+    factorial = i * factorial;
   }
   console.log(factorial);
   factorialPara.innerText = factorial;
 }
 
 function handleclick() {
-  findfactorial(Number(document.getElementById("number").value));
+  let x = input.value;
+  console.log(x);
+  if (isNaN(x)) {
+    console.log("Please enter a valid number");
+    window.alert("Please enter a valid number");
+    input.value = "";
+  } else {
+    findfactorial(x);
+  }
 }
