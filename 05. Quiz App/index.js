5ecee96076704000965bf09a0f579d7b80d21938
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-29 19:56:21 +0530

    initialized the array of Qsand Answers in JS

--- 05. Quiz App/index.js
+++ 05. Quiz App/index.js
@@ -1,24 +1,77 @@
-const factorialPara = document.getElementById("factorial");
-const input = document.getElementById("inputbox");
+const QnAs = [
+  {
+    Q_no: 01,
+    Question: " Is JavaScript one of the most awesome languages to learn? ",
+    AnswersArray: [
+      {
+        Maintext: "Yes ",
+        SubText: "it is just too awesome",
+      },
+      {
+        Maintext: "Its good ",
+        SubText: "but could be better",
+      },
+      {
+        Maintext: "Meh, ",
+        SubText: "its okay",
+      },
+      {
+        Maintext: "Nah  ",
+        SubText: "I dont like it at all",
+      },
+    ],
+    RightAnwerIndex: 2,
+  },
 
-function findfactorial(n) {
-  let factorial = 1;
+  {
+    Q_no: 02,
+    Question: " Is JavaScript one of the most awesome languages to learn? ",
+    AnswersArray: [
+      {
+        Maintext: "Yes ",
+        SubText: "it is just too awesome",
+      },
+      {
+        Maintext: "Its good ",
+        SubText: "but could be better",
+      },
+      {
+        Maintext: "Meh, ",
+        SubText: "its okay",
+      },
+      {
+        Maintext: "Nah  ",
+        SubText: "I dont like it at all",
+      },
+    ],
+    RightAnwerIndex: 3,
+  },
 
-  for (i = 1; i <= n; i++) {
-    factorial = i * factorial;
-  }
-  console.log(factorial);
-  factorialPara.innerText = factorial;
-}
+  {
+    Q_no: 03,
+    Question: " Is JavaScript one of the most awesome languages to learn? ",
+    AnswersArray: [
+      {
+        Maintext: "Yes ",
+        SubText: "it is just too awesome",
+      },
+      {
+        Maintext: "Its good ",
+        SubText: "but could be better",
+      },
+      {
+        Maintext: "Meh, ",
+        SubText: "its okay",
+      },
+      {
+        Maintext: "Nah  ",
+        SubText: "I dont like it at all",
+      },
+    ],
+    RightAnwerIndex: 0,
+  },
+];
 
-function handleclick() {
-  let x = input.value;
-  console.log(x);
-  if (isNaN(x)) {
-    console.log("Please enter a valid number");
-    window.alert("Please enter a valid number");
-    input.value = "";
-  } else {
-    findfactorial(x);
-  }
-}
+// const input = document.getElementById("inputbox");
+
+function handleclick() {}
