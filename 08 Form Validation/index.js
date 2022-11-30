e07e7afc7b0d4b0b8d1d4ae5fe3932d9e9dc03a1
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-30 15:28:08 +0530

    validation done

--- 08 Form Validation/index.js
+++ 08 Form Validation/index.js
@@ -6,6 +6,9 @@ const usernameFaliurePara = document.getElementById("usernameFaliurePara");
 const emailSuccessPara = document.getElementById("emailSuccessPara");
 const emailFaliurePara = document.getElementById("emailFaliurePara");
 
+console.log(inputUsername);
+console.log(inputEmail);
+
 let greenclass =
   "bg-green-50 border border-green-500 text-green-900 placeholder-green-700  focus:ring-green-500 focus:border-green-500";
 
@@ -15,16 +18,21 @@ redclass = String(
   "bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:bg-red-100 dark:border-red-400"
 );
 
-inputUsername.addEventListener("onkeyup", () => {
+function keyupFunc() {
+  console.log("keyup");
   console.log(inputUsername.value);
   if (inputUsername.value.length >= 8) {
-    inputUsername.classList.add();
+    inputUsername.classList.add("greenclass");
     usernameSuccessPara.classList.remove("hidden");
+    usernameFaliurePara.classList.add("hidden");
   } else {
-    inputUsername.classList.add(redclass);
+    inputUsername.classList.add("redclass");
     usernameFaliurePara.classList.remove("hidden");
+    usernameSuccessPara.classList.add("hidden");
   }
-});
+}
+
+inputUsername.addEventListener("keyup", keyupFunc);
 
 inputEmail.addEventListener("onkeyup", () => {
   console.log("keyup");
@@ -36,3 +44,7 @@ inputEmail.addEventListener("onkeyup", () => {
     emailFaliurePara.classList.remove("hidden");
   }
 });
+
+function handleclick() {
+  console.log("yolo");
+}
