00ea53290ffc908d7774646b9cc04c8bee10dc65
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-30 12:47:38 +0530

    ok

---
+++ 09. Array Methods/index.js
@@ -0,0 +1,62 @@
+// Find frequency of characters/ words in an array.
+
+const fruits = [Mango, Apple, Banana, Grapes];
+const fruitsObj = {
+  fruit1: Mango,
+  fruit2: Apple,
+  fruit3: Banana,
+  fruit4: Grapes,
+};
+
+//  String = paracetamol;
+
+// node index.js for running the code in the VS Code or any terminal
+console.log("Hi");
+
+let str = "aaabcdee";
+
+const arr = str.split(); // gives the seperate alphabets of the string in an array
+const charArr = [{ char: "", count: 0 }];
+
+// Display the frequency of occurance of each character
+
+// charactersObj = [
+//   {
+//     index: 0,
+//     value: "",
+//     Count: 0,
+//   },
+//   {
+//     index: 1,
+//     value: "",
+//     Count: 0,
+//   },
+//   {
+//     index: 2,
+//     value: "",
+//     Count: 0,
+//   },
+// ];
+
+for (i = 0; i < arr.length; i++) {
+  //   index x = find in charactersObj(arr[i]);
+}
+
+// = no rep of arr
+// mainly for object and array that is iterables
+
+// charactersObj.
+
+for (let char of str) {
+  // if it exists in charArr then increase its count
+  if (charArr.char(includes(char))) {
+    charArr[indexof(charArr.char)].count++;
+  }
+  // else (push the element in char array)
+  charArr.push({ char: char, count: 1 });
+
+  //   charArr.push({ char: char, count: previouscount++ });
+  //   if (!charArr.char(includes(char))) {
+
+  //   }
+}
